5675b8a472e64a84f2f26e7fccb365221157ac79
Leonardo Cespedes <leonardo.cespedes@gmail.com>
2023-03-05 14:09:39 -0500

    Se modifica ReceiveMessage para recibir mensajes de whatsapp

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,15 +1,12 @@
 const { response } = require("express");
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logs,txt"));
 
 const VerifyToken = (req, res = response) => {
     try {
         const accessToken = process.env.TOKEN;
         const token = req.query["hub.verify_token"];
         const challenge = req.query["hub.challenge"];
-
-        console.log(accessToken);
-        console.log(token);
-        console.log(challenge);
-        
 
         if (!challenge || !token || token !== accessToken) {
             res.status(400).send();
@@ -22,7 +19,19 @@ const VerifyToken = (req, res = response) => {
 }
 
 const ReceiveMessage = (req, res = response) => {
-    res.send("ReceiveMessage");
+    try {
+        const entry = (req.body["entry"])[0];
+        const changes = (entry["changes"])[0];
+        const value = changes["value"];
+        const messageObject = value["messages"];
+
+        myConsole.log(messageObject);
+
+        res.send("EVENT_RECEIVED");
+    } catch (error) {
+        myConsole.log(error);
+        res.send("EVENT_RECEIVED");
+    }
 }
 
 module.exports = {
