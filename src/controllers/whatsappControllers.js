5b095b24a3261a57765d1148003412e8304055bc
Leonardo Cespedes <leonardo.cespedes@gmail.com>
2023-03-05 11:57:16 -0500

    Se obtiene el token para verificar en el metodo verifyToken

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,7 +1,19 @@
 const { response } = require("express");
 
 const VerifyToken = (req, res = response) => {
-    res.send("verifyToken");
+    try {
+        const accessToken = process.env.TOKEN;
+        const token = req.query["hub.verify-tken"];
+        const challenge = req.body["hub.challenge"];
+
+        if (!challenge || !token || token !== accessToken) {
+            res.status(400).send();
+        }
+
+        res.send(challenge);
+    } catch (error) {
+        res.status(400).send()
+    }
 }
 
 const ReceiveMessage = (req, res = response) => {
