57724f53d0e04b918913aff914e9395738e4fa36
Leonardo Cespedes <leonardo.cespedes@gmail.com>
2023-03-07 18:47:07 -0500

    Mensajes generales

--- src/shared/processMessage.js
+++ src/shared/processMessage.js
@@ -3,20 +3,25 @@ const { messageText } = require("./whatsappModel");
 
 const process = (textUser, number) => {
     textUser = textUser.toLowerCase();
+    console.log(textUser);
     const models = [] 
     let mensaje = ""
 
     if (textUser.includes("hola")) {
         mensaje = "Hola, un gusto saludarte";
+        console.log("Saludo - ", mensaje);
     } else if (textUser.includes("gracias")) {
         mensaje = "Gracias a ti por escribirme";
+        console.log("Agradecimiento - ", mensaje);
     } else if ( textUser.includes("adios") || 
                 textUser.includes("adiós") ||
                 textUser.includes("bye")   ||
                 textUser.includes("me voy") ) {
         mensaje = "Ve con cuidado";
+        console.log("Despedida - ", mensaje);
     } else {
         mensaje = "No entiendo lo que dices"
+        console.log("Perdido - ", mensaje);
     }
 
     const model = messageText(mensaje, number);
